1707ba3298a9a2cef08d0d73a1b8ac950890b5c8
Adam Nagy <adam.nagy@autodesk.com>
2018-02-22 14:40:38 +0000

    cleanup

--- server/model.derivative.js
+++ server/model.derivative.js
@@ -88,67 +88,6 @@ router.get('/metadatas/:urn', function (req, res) {
 });
 
 /////////////////////////////////////////////////////////////////
-// Get the hierarchy information for the model with the given
-// guid inside the file with the provided urn
-/////////////////////////////////////////////////////////////////
-router.get('/hierarchy', function (req, res) {
-    var derivatives = new forgeSDK.DerivativesApi();
-
-    var tokenSession = new token(req.session);
-
-    derivatives.getModelviewMetadata(req.query.urn, req.query.guid, {}, tokenSession.getInternalOAuth(), tokenSession.getInternalCredentials())
-        .then(function (metaData) {
-            if (metaData.body.data) {
-                res.json(metaData.body);
-            } else {
-                res.json({result: 'accepted'});
-            }
-        })
-        .catch(function (error) {
-            res.status(error.statusCode).end(error.statusMessage);
-        });
-});
-
-/////////////////////////////////////////////////////////////////
-// Get the properties for all the components inside the model
-// with the given guid and file urn
-/////////////////////////////////////////////////////////////////
-router.get('/properties', function (req, res) {
-    var derivatives = new forgeSDK.DerivativesApi();
-
-    var tokenSession = new token(req.session);
-
-    derivatives.getModelviewProperties(req.query.urn, req.query.guid, {}, tokenSession.getInternalOAuth(), tokenSession.getInternalCredentials())
-        .then(function (data) {
-            res.json(data.body);
-        })
-        .catch(function (error) {
-            res.status(error.statusCode).end(error.statusMessage);
-        });
-});
-
-/////////////////////////////////////////////////////////////////
-// Download the given derivative file, e.g. a STEP or other
-// file format which are associated with the model file
-/////////////////////////////////////////////////////////////////
-router.get('/download', function (req, res) {
-    var derivatives = new forgeSDK.DerivativesApi();
-
-    var tokenSession = new token(req.session);
-
-    derivatives.getDerivativeManifest(req.query.urn, req.query.derUrn, {}, tokenSession.getInternalOAuth(), tokenSession.getInternalCredentials())
-        .then(function (data) {
-            var fileExt = req.query.fileName.split('.')[1];
-            res.set('content-type', 'application/octet-stream');
-            res.set('Content-Disposition', 'attachment; filename="' + req.query.fileName + '"');
-            res.end(data.body);
-        })
-        .catch(function (error) {
-            res.status(error.statusCode).end(error.statusMessage);
-        });
-});
-
-/////////////////////////////////////////////////////////////////
 // Send a translation request in order to get an SVF or other
 // file format for our file
 /////////////////////////////////////////////////////////////////
